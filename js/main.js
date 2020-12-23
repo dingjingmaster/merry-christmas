f5c4f9b140726c63222f6acb87195f5004d31ddd
dingjingmaster <dingjing@live.cn>
2020-12-23 10:55:05 +0800

    暂存

--- js/main.js
+++ js/main.js
@@ -1,19 +1,3 @@
-/****************************************************************
- *																*		
- * 						      代码库							*
- *                        www.dmaku.com							*
- *       		  努力创建完善、持续更新插件以及模板			*
- * 																*
-****************************************************************/
-/**
- * http://www.codrops.com
- *
- * Licensed under the MIT license.
- * http://www.opensource.org/licenses/mit-license.php
- * 
- * Copyright 2013, Codrops
- * http://www.codrops.com
- */
 (function() {
 
 	'use strict';
